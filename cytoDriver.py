6cb15d5f3f45009e6e3d2c837ceb6955b1fb677f
Howard <howard@renci.org>
2023-11-09 13:44:33 -0500

    Added the ability to specify a targetNode.  This will only show links connected to that node

--- cytoDriver.py
+++ cytoDriver.py
@@ -12,8 +12,12 @@ parser = argparse.ArgumentParser()
 
 # set up the arguement parsing and extract the args
 parser.add_argument("--sifFile", help="sif formatted network file", required=True)
+parser.add_argument("--targetNode", help="Node to show links for", required=False)
 args = parser.parse_args()
 sifFile = args.sifFile
+targetNode = args.targetNode
+targetNodeId = None
+print(f"targetNode is {targetNode}")
 
 # set up the network URL
 cytoscape_url = "http://localhost:1234/v1"
@@ -101,12 +105,20 @@ nodes_data = nodes_response.json()
 print(f"nodes_data is {nodes_data}")
 print(len(nodes_data))
 for node in nodes_data:
+   nLinksUrl = f"{cytoscape_url}/networks/{thisNetwork}/nodes/{node}/adjEdges"
+   list_of_links = requests.get(nLinksUrl)
+   #print(f"list_of_links is {list_of_links.json()}")
+   #print(f"nLinks is {len(list_of_links.json())}")
+
    node_data = requests.get(f"{cytoscape_url}/networks/{thisNetwork}/nodes/{node}")
    node_data = node_data.json()
-   print(f"node_data is {node_data}")
+   #print(f"node_data is {node_data}")
    nodeName = node_data['data']['name']
    nodeSUID = node_data['data']['SUID']
    print(f"nodeName, nodeSUID are {nodeName} {nodeSUID}")
+   if nodeName == targetNode:
+      targetNodeId = nodeSUID
+      print(f"targetNodeId is {targetNodeId}")
 
    nodeNameProp = {
           'visualProperty': 'NODE_LABEL',
@@ -115,24 +127,24 @@ for node in nodes_data:
 
    nodeFontProp = {
           'visualProperty': 'NODE_LABEL_FONT_SIZE',
-          'value': 3
+          'value': 6
     } 
 
    putURL = f"{cytoscape_url}/networks/{thisNetwork}/views/{viewSUID}/nodes/{nodeSUID}?bypass=true"
-   print(f"putURL for nodes {putURL}")
+   #print(f"putURL for nodes {putURL}")
    nodePropArray = []
    nodePropArray.append(nodeNameProp)
    nodePropArray.append(nodeFontProp)
    formattedProps = json.dumps(nodePropArray[0], indent = 3)
-   print(f"formattedProps {formattedProps}")
+   #print(f"formattedProps {formattedProps}")
    reply = requests.put(putURL, json = nodePropArray)
-   print(f"node reply is {reply}")
+   #print(f"node reply is {reply}")
 
 # get the list of edges
 edges_response = requests.get(f"{cytoscape_url}/networks/{thisNetwork}/edges")
 print (edges_response)
 edges_data = edges_response.json()
-print(f"edges_data is {edges_data}")
+#print(f"edges_data is {edges_data}")
 print(len(edges_data))
 
 edge_thickness = {}
@@ -140,25 +152,27 @@ concept_list = {}
 for edge in edges_data:
    edge_data = requests.get(f"{cytoscape_url}/networks/{thisNetwork}/edges/{edge}")
    edge_data = edge_data.json()
-   print(f"edges_data is {edge_data}")
+   #print(f"edges_data is {edge_data}")
    source = edge_data["data"]["source"]
    target = edge_data["data"]["target"]
    pair = (source, target)
-   edge_thickness[pair] = edge_thickness.get(pair, 0) + .5
-   print(f"edge thickness of pair {source} {target} is {edge_thickness[pair]}")
+   edge_thickness[pair] = edge_thickness.get(pair, 0) + .2
+   #print(f"edge thickness of pair {source} {target} is {edge_thickness[pair]}")
    if pair not in concept_list:
       concept_list[pair] = []
-   print(f"appending concept {edge_data['data']['shared interaction']} for pair {source} {target}")
+#   print(f"appending concept {edge_data['data']['shared interaction']} for pair {source} {target}")
    concept_list[pair].append(edge_data["data"]["shared interaction"])
 
 reply = requests.delete(f"{cytoscape_url}/networks/{thisNetwork}/edges/")
-print(f"delete reply is {reply}")
+#print(f"delete reply is {reply}")
 
 time.sleep(0.1)
 
 for pair, thickness in edge_thickness.items():
     source, target = pair
-    #print(f"found {source} {target}")
+    print(f"found {source} {target}")
+    if targetNodeId != None and source != targetNodeId and target != targetNodeId:
+       continue
     new_edge_array = []
     new_edge_data = {
         "source": source,
@@ -171,15 +185,22 @@ for pair, thickness in edge_thickness.items():
     #print(f"repost reply is {reply}")
     replyJSON = reply.json()
     #print(f"repost replyJSON is {replyJSON }")
-    print(f"thickness is {thickness }")
+    #print(f"thickness is {thickness }")
 
     visualProp = {
           "visualProperty": "EDGE_WIDTH",
           "value": thickness
     } 
 
+    visualFontProp = {
+          'visualProperty': 'EDGE_LABEL_FONT_SIZE',
+          'value': 2
+    } 
+
+
     visualPropArray = []
     visualPropArray.append(visualProp)
+    visualPropArray.append(visualFontProp)
 
     conceptArray = concept_list[pair]
     conceptString = "\n".join([str(item) for item in conceptArray])
@@ -191,10 +212,10 @@ for pair, thickness in edge_thickness.items():
   
     edgeSUID = replyJSON[0]['SUID']
     putURL = f"{cytoscape_url}/networks/{thisNetwork}/views/{viewSUID}/edges/{edgeSUID}"
-    print(f"putURL {putURL}")
+    #print(f"putURL {putURL}")
     formattedProps = json.dumps(visualPropArray[0], indent = 3)
    # print(f"formattedProps {formattedProps}")
 
     reply = requests.put(putURL, json = visualPropArray)
-    print(f"visualProp reply is {reply}")
-    time.sleep(0.1)
+    #print(f"visualProp reply is {reply}")
+    time.sleep(0.05)
